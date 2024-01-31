1e389d8937d895fd61665ff8f610214ab8506ead
Saksham Katiyar <115263557+SakshamKatiy@users.noreply.github.com>
2024-01-31 11:39:32 +0530

    insert muli..doc and read

--- src/app.js
+++ src/app.js
@@ -25,25 +25,73 @@ mongoose.connect("mongodb://localhost:27017/expressData").then(()=>{
         }
     });
 
-    // create collection
+    // 
+// const playlist = new mongoose.model("playlist",playlistSchema);
+
+// // create document or insert
+// const createDocument = async()=>{
+// try{
+//     const newPlaylist = new playlist({
+//         name:"Node js",
+//         ctype:"Backend",
+//         videos:50,
+//         author:"Saksham",
+//         active:true
+//     });
+    
+//     const result = await newPlaylist.save()
+//     console.log(result);
+// }catch(err){
+//     console.log(err);
+// }
+// }
+// createDocument();
+
+
+
+//   Inserting multiple documents---------
 const playlist = new mongoose.model("playlist",playlistSchema);
 
 // create document or insert
 const createDocument = async()=>{
 try{
-    const newPlaylist = new playlist({
-        name:"Node js",
-        ctype:"Backend",
-        videos:50,
-        author:"Saksham",
-        active:true
-    });
-    
-    const result = await newPlaylist.save()
-    console.log(result);
+const htmlPlaylist = new playlist({
+    name:"html",
+    ctype:"front end",
+    videos:1,
+    author:"Saksham",
+    active:true
+});
+
+const cssPlaylist = new playlist({
+    name:"CSS",
+    ctype:"front end",
+    videos:2,
+    author:"Saksham",
+    active:true
+});
+
+const jsPlaylist = new playlist({
+    name:"js",
+    ctype:"front end",
+    videos:3,
+    author:"Saksham",
+    active:true
+});
+const result = await playlist.insertMany([jsPlaylist,htmlPlaylist,cssPlaylist]);
+console.log(result);
 }catch(err){
-    console.log(err);
+console.log(err);
 }
 }
 createDocument();
 
+
+// create Document();
+const getDocument = async()=>{
+    const result = await playlist.find({ctype:"font end"})
+    .select({name:1})
+    .limit(1)
+    console.log();
+}
+getDocument();
