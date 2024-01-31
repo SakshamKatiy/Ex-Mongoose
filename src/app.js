042e1fd84440f7f100f932645c622c2b3c27bdf8
Saksham Katiyar <115263557+SakshamKatiy@users.noreply.github.com>
2024-01-31 14:48:35 +0530

    crud completed

--- src/app.js
+++ src/app.js
@@ -53,45 +53,115 @@ mongoose.connect("mongodb://localhost:27017/expressData").then(()=>{
 const playlist = new mongoose.model("playlist",playlistSchema);
 
 // create document or insert
-const createDocument = async()=>{
-try{
-const htmlPlaylist = new playlist({
-    name:"html",
-    ctype:"front end",
-    videos:1,
-    author:"Saksham",
-    active:true
-});
+// const createDocument = async()=>{
+// try{
+// const htmlPlaylist = new playlist({
+//     name:"html",
+//     ctype:"front end",
+//     videos:1,
+//     author:"Saksham",
+//     active:true
+// });
 
-const cssPlaylist = new playlist({
-    name:"CSS",
-    ctype:"front end",
-    videos:2,
-    author:"Saksham",
-    active:true
-});
+// const cssPlaylist = new playlist({
+//     name:"CSS",
+//     ctype:"front end",
+//     videos:2,
+//     author:"Saksham",
+//     active:true
+// });
 
-const jsPlaylist = new playlist({
-    name:"js",
-    ctype:"front end",
-    videos:3,
-    author:"Saksham",
-    active:true
-});
-const result = await playlist.insertMany([jsPlaylist,htmlPlaylist,cssPlaylist]);
-console.log(result);
+// const jsPlaylist = new playlist({
+//     name:"js",
+//     ctype:"front end",
+//     videos:3,
+//     author:"Saksham",
+//     active:true
+// });
+// const result = await playlist.insertMany([jsPlaylist,htmlPlaylist,cssPlaylist]);
+// console.log(result);
+// }catch(err){
+// console.log(err);
+// }
+// }
+// createDocument();
+
+
+// create Document();
+// const getDocument = async()=>{
+    //try{
+//     const result = await playlist.find({ctype:"font end"})
+//     .select({name:1})
+//     .limit(1)
+//     console.log(result);
+// }catch(err){
+// console.log(err);
+//}
+//}
+
+
+// comparison operator
+
+const getDocument = async()=>{
+    try{
+    const result = await playlist.find({videos:{$gt : 10}})
+    .select({name:1})
+    //.limit(1)
+    console.log(result);
 }catch(err){
 console.log(err);
 }
 }
-createDocument();
-
-
-// create Document();
-const getDocument = async()=>{
-    const result = await playlist.find({ctype:"font end"})
-    .select({name:1})
-    .limit(1)
-    console.log();
-}
 getDocument();
+
+// comparison operator
+//.find({$or:[{ctype:"angular"}{
+// author:"node js"
+//}]})
+
+// .countDocuments(); to count the document
+// .sort({name:-1}); to reverse the result
+
+
+// update document
+// const updateDocument = async(_id)=>{
+//     try{
+//         const result = await playlist.updateOne({_id},{
+//             $set : {
+//         name:"adit"
+//             }
+//          }   );
+//         console.log(result);
+//     }catch(error){
+//         console.log(err);
+
+//     }
+// }
+const updateDocument = async(_id)=>{
+    try{
+        const result = await playlist.findByIdAndUpdate({_id},{
+            $set : {
+        name:"adit trade"
+            }
+         }   );
+        console.log(result);
+    }catch(error){
+        console.log(err);
+
+    }
+}
+updateDocument('65b9d874f2411ab1f19c88ea');
+
+
+
+// deleteDocument
+
+const deleteDocument=async(_id)=>{
+    try{
+        const result = await playlist.findByIdAndDelete({_id });
+    }catch(err){
+        console.log(err)
+    }
+    
+}
+deleteDocument('65b9d874f2411ab1f19c88ea');
